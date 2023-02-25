4f49ad7a8b0f7ae0bc7d5111782a7b4333be98e6
Erik Bigler <erikbigler@gmail.com>
2023-02-25 09:47:02 -0600

    Update contributors

--- constants/people.ts
+++ constants/people.ts
@@ -8,61 +8,71 @@ export const people: Person[] = [
     {
         id: '22639237',
         name: 'Firebottle',
-        role: 'Creator / Developer'
+        role: 'Creator / Developer',
     },
     {
         id: '58612601',
         name: 'ebiggz',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '26831599',
         name: 'SReject',
-        role: 'Developer'
+        role: 'Developer',
+    },
+    {
+        id: '50989853',
+        name: 'zunderscore',
+        role: 'Developer',
     },
     {
         id: '90428736',
         name: 'ThePerry',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '443615007',
         name: 'CaveMobster',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '37677315',
         name: 'Heyaapl',
-        role: 'Tutorials / Support'
+        role: 'Tutorials / Support',
     },
     {
-        id: '140305832',
-        name: 'CorporalGigglesworth',
-        role: 'Support'
+        id: '79854720',
+        name: 'C_K_Y',
+        role: 'Support / Developer',
     },
     {
         id: '137263225',
         name: 'DragynsLair',
-        role: 'Support / Tester'
+        role: 'Support / Tester',
+    },
+    {
+        id: '140305832',
+        name: 'CorporalGigglesworth',
+        role: 'Support',
     },
     {
         id: '58680725',
         name: 'baamang',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '24257728',
         name: 'Kateract',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '546820778',
         name: 'Senepa',
-        role: 'Developer'
+        role: 'Developer',
     },
     {
         id: '26797733',
         name: 'Skriglitz',
-        role: 'Developer'
-    }
+        role: 'Developer',
+    },
 ];
