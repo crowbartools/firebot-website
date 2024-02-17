466105446765cde304cf4b4672c4f15718e8f3ec
Erik Bigler <erikbigler@gmail.com>
2024-02-17 06:34:54 -0600

    dont use connection string

--- backend/db.ts
+++ backend/db.ts
@@ -1,22 +1,15 @@
 import { Pool } from 'pg';
 
-const pool = new Pool(
-    process.env.DATABASE_URL
-        ? {
-              connectionString: process.env.DATABASE_URL,
-              ssl: { rejectUnauthorized: false },
-          }
-        : {
-              host: process.env.POSTGRES_HOST,
-              port: parseInt(process.env.POSTGRES_PORT, 10),
-              user: process.env.POSTGRES_USER,
-              password: process.env.POSTGRES_PASSWORD,
-              database: 'firebot',
-              ssl: {
-                  rejectUnauthorized: false,
-              },
-          }
-);
+const pool = new Pool({
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'firebot',
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 export const query = async (text: string, params?: any[]) => {
     try {
